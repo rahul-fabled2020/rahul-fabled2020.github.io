7f9425af0f16b2e906ffcc604a930aaa1afd0ee7
Rahul Sharma <rahul.fabled@gmail.com>
2020-07-19 02:22:06 +0545

    Refactor Bowser code

--- leapfrog/JS/FinalProject/js/components/enemies/Bowser.js
+++ leapfrog/JS/FinalProject/js/components/enemies/Bowser.js
@@ -1,3 +1,9 @@
+/**
+ * Bowser class is a subclass of Enemy class
+ * position is the position of the object to be rendered in the canvas.
+ * position is a 2D Vector representing (x, y) where x is the distance from the left edge of the canvas and y is the distance from the top edge of the canvas
+ * level is the level in which the enemy is to be rendered
+ */
 class Bowser extends Enemy {
   constructor(position, level) {
     super({
@@ -15,30 +21,42 @@ class Bowser extends Enemy {
     this.shm = new SHM(this.position, this.velocity, 32);
     this.velocity.x = 1;
     this.acceleration.y = 0.2;
+
     this.canJump = true;
     this.jumpTime = BOWSER_JUMP_TIME;
     this.jumpCountDownTime = BOWSER_JUMP_INTERVAL;
     this.fireTime = FIRE_TIME;
     this.mouthOpenTime = MOUTH_OPEN_TIME;
+
     this.weapon = [];
     this.weaponIndex = 0;
   }
 
+  /**
+   * 
+   * @param {number} dt 
+   * @param {Camera} camera 
+   * @param {Mario} player 
+   * @param {number} gameTime 
+   */
   update(dt, camera, player, gameTime) {
-    if (player.powerTime) return;
+    if (player.powerTime) return; //If the player is changing its state then pause the enemy
 
-    if (this.position.x - camera.x > 21 * TILE_SIZE) return;
+    if (this.position.x - camera.x > 21 * TILE_SIZE) return; //Stop rendering if the enemy is not in the camera viewport
 
+    //Delete the enemy when we have left the enemy behind
     if (this.position.x - camera.x < -2 * TILE_SIZE) {
       delete this.level.enemies[this.index];
     }
 
+    //For turning the enemy left/right
     if (this.velocity.x > 0) {
       this.sprite.imageUrl = ENEMIES_RIGHT;
     } else {
       this.sprite.imageUrl = ENEMIES_LEFT;
     }
 
+    //Manage die timer
     if (this.isDyingCount) {
       this.isDyingCount--;
 
@@ -51,41 +69,53 @@ class Bowser extends Enemy {
     this.jumpCountDownTime -= dt;
     this.mouthOpenTime -= dt;
 
+    //Manage fire timer
     if (this.fireTime <= 0) {
       this.fire();
       this.fireTime = FIRE_TIME;
     }
 
+    //Manage mouth open timer
     if(this.mouthOpenTime <=0 ){
       this.sprite.position.x = 47 * TILE_SIZE;
       this.mouthOpenTime = MOUTH_OPEN_TIME;
     }
 
+    //Jump on time up for jump timer
     if (this.jumpCountDownTime <= 0) {
       this.jump();
     }
 
     this.noJump();
+
+    //Update enemey position
     this.velocity.y += this.acceleration.y;
-    // this.position = this.position.add(this.velocity);
     this.position.y += this.velocity.y;
     this.shm.update(dt, this.position);
     this.sprite.update(dt, gameTime);
   }
 
+  /**
+   * It projects the enemy's weapon
+   */
   fire() {
     SOUND.bowserfire.play();
 
     this.sprite.position.x = 43 * TILE_SIZE;
     this.sprite.animationSpeed = 5;
+
     let weapon = new Fire(
       this.position.subtract(new Vector(TILE_SIZE, -0.25 * TILE_SIZE)),
       this
     );
+
     this.weaponIndex = weapon.index;
     weapon.spawn();
   }
 
+  /**
+   * Enemy jumps
+   */
   jump() {
     if (this.velocity.y > 0) {
       return;
@@ -104,6 +134,9 @@ class Bowser extends Enemy {
     }
   }
 
+  /**
+   * Resets jump
+   */
   noJump() {
     this.canJump = true;
     this.sprite.animationSpeed = 5;
