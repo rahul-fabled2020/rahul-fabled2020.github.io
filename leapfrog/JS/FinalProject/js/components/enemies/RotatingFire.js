f5566b5b1a25107d5b9a3734508384fa891984fa
Rahul Sharma <rahul.fabled@gmail.com>
2020-07-19 11:21:05 +0545

    Refactor RotatingFire code

--- leapfrog/JS/FinalProject/js/components/enemies/RotatingFire.js
+++ leapfrog/JS/FinalProject/js/components/enemies/RotatingFire.js
@@ -1,3 +1,10 @@
+/**
+ * Rotating Firebar is the obstacle in the castle put by Bowser on the way so that Mario cannot reach upto him and save the princess
+ * @param {Vector} position
+ * @param {number} angle; Initial angle of rotation
+ * @param {Vector} center; Center of rotation or the pivot point
+ * @param {number} distanceFromCenter; How far to put each component of the firebar from the center of rotation
+ */
 class RotatingFire extends Entity {
   constructor(position, angle, center, distanceFromCenter) {
     super({
@@ -22,6 +29,11 @@ class RotatingFire extends Entity {
     this.distanceFromCenter = distanceFromCenter;
   }
 
+  /**
+   * Update the rotating firebar
+   * @param {number} dt 
+   * @param {number} gameTime 
+   */
   update(dt, gameTime) {
     let rotationSpeed = 100;
 
@@ -31,9 +43,11 @@ class RotatingFire extends Entity {
       this.rotationAngle = 0;
     }
 
+    //Using sine and cosine functions for the circular motion or the periodic motion
     this.position.x =
       this.centerOfRotation.x +
       Math.cos(this.rotationAngle * DEGREE) * this.distanceFromCenter;
+
     this.position.y =
       this.centerOfRotation.y +
       Math.sin(this.rotationAngle * DEGREE) * this.distanceFromCenter;
@@ -41,15 +55,26 @@ class RotatingFire extends Entity {
     this.sprite.update(dt, gameTime);
   }
 
+  /**
+   * Check collision with the player
+   * @param {Camera} camera 
+   * @param {Mario} player 
+   */
   detectCollision(camera, player) {
-    if (player.dyingTime || player.powerTime) return;
+    if (player.dyingTime || player.powerTime) return; //No need to check for collision when the player is dying or powering up/down
 
     this.isCollidingWith(player);
   }
 
+  /**
+   * 
+   * @param {Entity} entity; instance of Mario
+   */
   isCollidingWith(entity) {
-    let entityHLeft = entity.position.x + entity.hitbox.x;
-    let entityHTop = entity.position.y + entity.hitbox.y;
+    let entityHLeft = entity.position.x + entity.hitbox.x; //Left edge of the hitbox
+    let entityHTop = entity.position.y + entity.hitbox.y; //Top edge of the hitbox
+
+    //Center of the hitbox
     let entityHCenter = new Vector(
       entityHLeft + entity.hitbox.width / 2,
       entityHTop + entity.hitbox.height / 2
@@ -57,14 +82,17 @@ class RotatingFire extends Entity {
 
     let thisHLeft = this.position.x + this.hitbox.x;
     let thisHTop = this.position.y + this.hitbox.y;
+
     let thisHCenter = new Vector(
       thisHLeft + this.hitbox.width / 2,
       thisHTop + this.hitbox.height / 2
     );
 
+    //Average dimensions of the hitboxes
     let averageWidth = (entity.hitbox.width + this.hitbox.width) / 2;
     let averageHeight = (entity.hitbox.height + this.hitbox.height) / 2;
 
+    //Center to center distance vector
     let displacement = entityHCenter.subtract(thisHCenter);
 
     //If either displacement.x or displacement.y is greater than the average dimension then there is no collision
