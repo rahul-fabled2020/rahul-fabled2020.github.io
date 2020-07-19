b48b3f943c296190d60b735e47746412ba33fe70
Rahul Sharma <rahul.fabled@gmail.com>
2020-07-19 11:54:10 +0545

    Refactor Enemy code

--- leapfrog/JS/FinalProject/js/components/enemies/Enemy.js
+++ leapfrog/JS/FinalProject/js/components/enemies/Enemy.js
@@ -1,3 +1,7 @@
+/**
+ * The generic enemy class
+ * @param {Object} configuration; Contains enemy position, sprite, hitbox and level instances
+ */
 class Enemy extends Entity {
   constructor(configuration) {
     super({
@@ -6,28 +10,40 @@ class Enemy extends Entity {
       hitbox: configuration.hitbox,
     });
 
-    this.isDyingCount = 0;
-    this.velocity = new Vector(-0.5, 0);
+    this.velocity = new Vector(-0.5, 0); //Initially the enemy moves towards left direction
+
+    this.isDyingCount = 0; //To maintain death timer
+    this.isFlipping = false; //To maintain bullet collision status
+
     this.level = configuration.level;
     this.index = this.level.enemies.length;
-    this.isFlipping = false;
+    
   }
 
+  /**
+   * Update the Enemy
+   * @param {number} dt 
+   * @param {Camera} camera 
+   * @param {Mario} player 
+   * @param {number} gameTime 
+   */
   update(dt, camera, player, gameTime) {
-    if (player.powerTime || player.dyingTime) return;
+    if (player.powerTime || player.dyingTime) return; //Pause enemy when mario is power up/down or dying.
 
-    if (this.position.x - camera.x > 21 * TILE_SIZE) return;
+    if (this.position.x - camera.x > 21 * TILE_SIZE) return; //Pause enemy when the player is too far from it
 
     if (this.position.x - camera.x < -2 * TILE_SIZE) {
-      delete this.level.enemies[this.index];
+      delete this.level.enemies[this.index]; //Delete enemies when the player leaves it behind
     }
 
+    //Change the tilesheet to match enemy orientation
     if (!(this instanceof Goomba) && this.velocity.x > 0) {
       this.sprite.imageUrl = ENEMIES_RIGHT;
     } else {
       this.sprite.imageUrl = ENEMIES_LEFT;
     }
 
+    //Manage death timer
     if (this.isDyingCount) {
       this.isDyingCount--;
 
@@ -36,13 +52,20 @@ class Enemy extends Entity {
       }
     }
 
+    //Update position and velocity
     this.acceleration.y = 0.2;
     this.velocity.y += this.acceleration.y;
     this.position = this.position.add(this.velocity);
 
+    //Update the sprite
     this.sprite.update(dt, gameTime);
   }
 
+  /**
+   * Check collision with entities
+   * @param {Camera} camera 
+   * @param {Mario} player 
+   */
   detectCollision(camera, player) {
     if (this.isFlipping) return;
 
@@ -58,26 +81,27 @@ class Enemy extends Entity {
       w++;
     }
 
-    if (this instanceof Koopa || this instanceof Bowser || HammerBro) h++;
+    if (this instanceof Koopa || this instanceof Bowser || HammerBro) h++; //Koopa, Bowser and HammerBro occupy more than one TILE_SIZE height
 
-    if (this instanceof Bowser) w++;
+    if (this instanceof Bowser) w++; //Bowser occupies more than one TILE_SIZE width
 
-    let baseX = Math.floor(this.position.x / TILE_SIZE);
-    let baseY = Math.floor(this.position.y / TILE_SIZE);
+    let baseX = Math.floor(this.position.x / TILE_SIZE); //Tile position.x of the top left vertex of the enemy container box
+    let baseY = Math.floor(this.position.y / TILE_SIZE); //Tile position.y of the top left vertex of the enemy container box
 
+    //Delete the enemy if it is below the viewport height
     if (baseY + h > MAX_ROW_SIZE) {
       delete this.level.enemies[this.index];
       return;
     }
 
-    //With floor and blocks
+    //With floor, pipes and blocks
     for (let i = 0; i < h; i++) {
       if (baseY + i < 0 || baseY + i >= MAX_ROW_SIZE) {
-        continue;
+        continue; //no need to detect collision for tiles above or below the viewport height
       }
 
       for (let j = 0; j < w; j++) {
-        if (baseY < 0) {
+        if (baseY < 0) { //no need to check for collision if the enemy is above the viewport height
           i++;
         }
 
@@ -106,15 +130,15 @@ class Enemy extends Entity {
 
     //With other enemies
     this.level.enemies.forEach((enemy) => {
-      if (enemy === this) return;
+      if (enemy === this) return; //No need to check collision with self
 
-      if (enemy.position.x - camera.x > 21 * TILE_SIZE) return;
+      if (enemy.position.x - camera.x > 21 * TILE_SIZE) return; //No need to check collision if the player is too far from the enemy
 
       this.isCollidingWith(enemy);
     });
 
     //With player
-    if(player.dyingTime || player.powerTime || this.isDyingCount) return;
+    if(player.dyingTime || player.powerTime || this.isDyingCount) return; //No need to check collision when the player is dying or powering or the enemy is dying
 
     this.isCollidingWith(player);
   }
@@ -123,6 +147,7 @@ class Enemy extends Entity {
 
     let entityHLeft = entity.position.x + entity.hitbox.x;
     let entityHTop = entity.position.y + entity.hitbox.y;
+
     let entityHCenter = new Vector(
       entityHLeft + entity.hitbox.width / 2,
       entityHTop + entity.hitbox.height / 2
@@ -130,6 +155,7 @@ class Enemy extends Entity {
 
     let thisHLeft = this.position.x + this.hitbox.x;
     let thisHTop = this.position.y + this.hitbox.y;
+
     let thisHCenter = new Vector(
       thisHLeft + this.hitbox.width / 2,
       thisHTop + this.hitbox.height / 2
@@ -138,7 +164,7 @@ class Enemy extends Entity {
     let averageWidth = (entity.hitbox.width + this.hitbox.width) / 2;
     let averageHeight = (entity.hitbox.height + this.hitbox.height) / 2;
 
-    let displacement = entityHCenter.subtract(thisHCenter);
+    let displacement = entityHCenter.subtract(thisHCenter); //Center to center displacement vector
 
     //If either displacement.x or displacement.y is greater than the average dimension then there is no collision
     if (
@@ -155,35 +181,38 @@ class Enemy extends Entity {
         if (displacement.x < 0) {
           //Entity is colliding from the left
           entity.velocity.x = - Math.abs(entity.velocity.x)
-          // entity.velocity.x = Math.min(0, entity.velocity.x);
-          // entity.acceleration.x = Math.min(0, entity.acceleration.x);
 
           entity.position.x = this.position.x - entity.hitbox.width;
         } else {
           //Entity is colliding from the right
           entity.velocity.x = Math.abs(entity.velocity.x)
-          // entity.acceleration.x = Math.max(0, entity.acceleration.x);
 
           entity.position.x = this.position.x + this.hitbox.width;
         }
       }
     }
+
     if (entity instanceof Mario) {
       if (entity.velocity.y > 0 && !(this instanceof Bowser)) {
         this.stomp(entity);
       } else {
         entity.getDamaged();
       }
-    } else {
-      // this.reverseHorizontalVelocity();
-      // entity.reverseHorizontalVelocity()
     }
   }
 
+  /**
+   * Reverse enemy velocity
+   */
   reverseHorizontalVelocity() {
     this.velocity.x *= -1;
   }
 
+  /**
+   * Initiate enemy's death when mario jumps on it.
+   * To be implemented by individual enemies
+   * @param {Mario} player 
+   */
   stomp(player) {
 
   }
